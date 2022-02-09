caa83a4588cffc834f856347be0a75f3c02113e3
ghost322-admin <lucaspiccirilli@gmail.com>
2022-02-08 22:25:11 -0300

    final deploy

---
+++ src/components/PxButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    @click="buttonClick"
+    class="bg-transparent hover:bg-orange-500 text-orange-700 font-semibold hover:text-white py-2 px-2 border border-orange-500 hover:border-transparent rounded"
+  >
+    <beat-loader :loading="isLoading" :color="'#FF9F45'" :size="8" />
+    <p v-show="!isLoading">
+      <slot></slot>
+    </p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'PxButton',
+
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    buttonClick() {
+      this.$emit('custom-click')
+    }
+  }
+}
+</script>
